285e5ec46b4ed3faa3b9248e0d9744177dd77870
Ricardo Novak <ricardonovak@yahoo.com.br>
2018-09-05 22:39:06 -0300

    First commit, glClear won't work, but cmake is almost working

---
+++ cmake/FindGLEW.cmake
@@ -0,0 +1,7 @@
+add_library(GLEW STATIC IMPORTED)
+target_include_directories(GLEW
+        INTERFACE
+        ${CMAKE_HOME_DIRECTORY}/libs/glew/include
+        )
+set_target_properties(GLEW PROPERTIES IMPORTED_LOCATION ${CMAKE_HOME_DIRECTORY}/libs/glew/lib/Release/x64/glew32.lib)
+set(GLEW_FOUND True)
---
+++ cmake/FindGLFW.cmake
@@ -0,0 +1,7 @@
+add_library(GLFW STATIC IMPORTED)
+target_include_directories(GLFW
+        INTERFACE
+        ${CMAKE_HOME_DIRECTORY}/libs/glfw/include
+        )
+set_target_properties(GLFW PROPERTIES IMPORTED_LOCATION ${CMAKE_HOME_DIRECTORY}/libs/glfw/lib-vc2015/glfw3.lib)
+set(GLFW_FOUND True)
