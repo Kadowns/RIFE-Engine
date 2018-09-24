8b3223c0526ecc86aeb7d9202cf03713d9ead01f
Ricardo Novak <ricardonovak@yahoo.com.br>
2018-09-23 23:09:06 -0300

    - starting rm! Rife Math!

---
+++ Program/Engine/Tools/Include/Rm.h
@@ -0,0 +1,57 @@
+#include <Types.h>
+#include <math.h>
+
+namespace rm {
+
+	class vec2f {
+	protected:
+
+		float64 m_x, m_y;
+
+	public:	
+		//Constructor
+		vec2f();
+		vec2f(const vec2f &vec);
+		vec2f(float64 x, float64 y);
+
+
+		//Getters
+		float64 x() { return m_x; }
+		float64 y() { return m_y; }
+		vec2f xy() { return *this; }
+		float64 magnitude();
+		float64 dot(vec2f vec);
+		vec2f cross(vec2f vec);
+		vec2f normalized();
+
+		//Methods
+		void normalize();
+		
+
+		//Operators
+		vec2f operator/(float64 scalar);
+		void operator/=(float64 scalar);
+
+	};
+
+	class vec3f : vec2f {
+	protected:
+
+		float64 m_z;
+
+	public:
+
+		//Constructors
+		vec3f(const vec3f &vec);
+		vec3f(const vec2f &vecxy, float64 z);
+		vec3f(float64 x, float64 y, float64 z);
+		vec3f();
+
+
+		//Getters
+		float64 z() { return m_z; }
+		vec2f xz() { return vec2f(m_x, m_z); }
+		vec2f yz() { return vec2f(m_y, m_z); }
+		vec3f xyz() { return *this; }
+	};
+}
---
+++ Program/Engine/Tools/Include/Types.h
@@ -0,0 +1,10 @@
+
+//Integers
+typedef int int32;
+typedef long int64;
+typedef long long int128;
+
+//Floating points
+typedef float float32;
+typedef double float64;
+typedef long double float128;
